5d3507d1d011cf7b0c9e266467eed08a0fb45754
Suyashyadav04 <160169351+Suyashyadav04@users.noreply.github.com>
2024-11-20 23:28:04 +0530

    Add files via upload
    thsese are the task of my internship

---
+++ yt Internship/scrap.py
@@ -0,0 +1,12 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import urllib.parse
+# def google_search(query, num_results= 10):
+#     base_url = "https://www.google.com/search"
+#     headers = {
+#         "User-Agent":
+#     }
+
+
+
+
